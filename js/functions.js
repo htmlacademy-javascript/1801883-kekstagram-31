d7d1ab326b237188f14bc8cc521e2f35c12f7c26
Vladislav <vlad.4sa@gmail.com>
2024-02-25 19:53:15 +0700

    Убираем комментарии, приводим функции к стрелочным

--- js/functions.js
+++ js/functions.js
@@ -1,28 +1,19 @@
-// Функция для проверки длины строки.
-// eslint-disable-next-line no-unused-vars
 const checkLength = (inputString, needLength) => (inputString.length <= needLength);
+checkLength('проверяемая строка', 18);
 
 
-// Функция для проверки, является ли строка палиндромом
-// eslint-disable-next-line no-unused-vars
-function isPalindrome(inputString) {
+const isPalindrome = (inputString) => {
   const normalizedString = inputString.replaceAll(' ', '').toLowerCase();
   let reverseString = '';
   for (let i = (normalizedString.length - 1); i >= 0; i--) {
     reverseString += normalizedString.at(i);
   }
   return reverseString === normalizedString;
-}
-/* Проверка
-console.log(isPalindrome('топот'));                      // true
-console.log(isPalindrome('ДовОд'));                      // true
-console.log(isPalindrome('Кекс'));                       // false
-console.log(isPalindrome('Лёша на полке клопа нашёл ')); // true*/
+};
+isPalindrome('Лёша на полке клопа нашёл ');
 
 
-// Функция извлечения цифр из строки
-// eslint-disable-next-line no-unused-vars
-function extractNumbers(inputString) {
+const extractNumbers = (inputString) => {
   if ((typeof inputString) === 'number') {
     inputString = inputString.toString();
   }
@@ -36,14 +27,7 @@ function extractNumbers(inputString) {
   }
 
   return parseInt(outputNumber, 10);
-}
-/* Проверка
-console.log(extractNumbers('2023 год'));            // 2023
-console.log(extractNumbers('ECMAScript 2022'));     // 2022
-console.log(extractNumbers('1 кефир, 0.5 батона')); // 105
-console.log(extractNumbers('агент 007'));           // 7
-console.log(extractNumbers('а я томат'));           // NaN
-console.log('-----------');
-console.log(extractNumbers(2023));                  // 2023
-console.log(extractNumbers(-1));                    // 1
-console.log(extractNumbers(1.5));                   // 15*/
+};
+
+extractNumbers('1 кефир, 0.5 батона');
+extractNumbers(-1.5);
